ff2c9ae29cb7a992065e2b6866a97005738ede65
winterbreaker <joejamesdev@hotmail.com>
2024-09-23 02:13:13 -0700

    Update utils.js

--- utils.js
+++ utils.js
@@ -73,3 +73,19 @@ async function getZkSyncProvider (zksync, networkName) {
     const feeInWei = await zkSyncProvider.getTransactionFee(transactionType, address, token)
     return ethers.utils.formatEther(feeInWei.totalFee.toString())
   }
+  
+  async function withdrawToEthereum (wallet, amountToWithdraw, withdrawalFee, token, zksync, ethers) {
+  
+    const closestPackableAmount = zksync.utils.closestPackableTransactionAmount(ethers.utils.parseEther(amountToWithdraw))
+    const closestPackableFee = zksync.utils.closestPackableTransactionFee(ethers.utils.parseEther(withdrawalFee))
+    const withdraw = await wallet.withdrawFromSyncToEthereum({
+      ethAddress: wallet.address(),
+      token: token,
+      amount: closestPackableAmount,
+      fee: closestPackableFee
+    })
+    await withdraw.awaitVerifyReceipt()
+    console.log('ZKP verification is complete')
+  
+  }
+  
