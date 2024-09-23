a83cff6b2b3be35fd23eeefa162d23f4dd630f0a
winterbreaker <joejamesdev@hotmail.com>
2024-09-23 02:32:36 -0700

    Update bob.js

--- src/bob.js
+++ src/bob.js
@@ -3,7 +3,7 @@
     const zksync = require('zksync')
     const utils = require('./utils')
   
-    const zkSyncProvider = await zksync.utils.getZkSyncProvider(zksync, process.env.NETWORK_NAME)
-    const ethersProvider = await ethers.utils.getEthereumProvider(ethers, process.env.NETWORK_NAME)
+    const zkSyncProvider = await utils.getZkSyncProvider(zksync, process.env.NETWORK_NAME)
+    const ethersProvider = await utils.getEthereumProvider(ethers, process.env.NETWORK_NAME)
   })()
   
