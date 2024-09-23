eb4d65df672f6cd3e4b70a6cbca737ac5e5c7904
winterbreaker <joejamesdev@hotmail.com>
2024-09-23 00:50:18 -0700

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,10 +1,28 @@
-async function getZkSyncProvider(zksync, networkName) {
-    let zkSyncProvider;
+async function getZkSyncProvider (zksync, networkName) {
+    let zkSyncProvider
     try {
-        zkSyncProvider = await zksync.getDefaultProvider(networkName)
+      zkSyncProvider = await zksync.getDefaultProvider(networkName)
     } catch (error) {
-        console.log('Unable to connect to zkSync.')
-        console.log(error)
+      console.log('Unable to connect to zkSync.')
+      console.log(error)
     }
     return zkSyncProvider
-}
+  }
+  
+  async function getEthereumProvider (ethers, networkName) {
+    let ethersProvider
+    try {
+      // eslint-disable-next-line new-cap
+      ethersProvider = new ethers.getDefaultProvider(networkName)
+    } catch (error) {
+      console.log('Could not connect to Rinkeby')
+      console.log(error)
+    }
+    return ethersProvider
+  }
+  
+  async function initAccount (rinkebyWallet, zkSyncProvider, zksync) {
+    const zkSyncWallet = await zksync.Wallet.fromEthSigner(rinkebyWallet, zkSyncProvider)
+    return zkSyncWallet
+  }
+  
